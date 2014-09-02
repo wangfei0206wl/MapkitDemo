215657a2593c26b5840509e52a25aba660dfea12
wangfei <wangfei0206wl@163.com>
2014-09-02 11:17:33 +0800

    create MapkitDemo project

---
+++ MapkitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapkitDemo
+//
+//  Created by wangfei on 14-9-2.
+//  Copyright (c) 2014年 busap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapkitDemoTests/MapkitDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  MapkitDemoTests.m
+//  MapkitDemoTests
+//
+//  Created by wangfei on 14-9-2.
+//  Copyright (c) 2014年 busap. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapkitDemoTests : XCTestCase
+
+@end
+
+@implementation MapkitDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
