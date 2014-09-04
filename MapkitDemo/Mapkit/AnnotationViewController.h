f5960c4cce1bca0c95676a4f0b287c50f1c1167c
wangfei <wangfei0206wl@163.com>
2014-09-04 16:28:35 +0800

    complete annotation demo

--- MapkitDemo/Mapkit/AnnotationViewController.h
+++ MapkitDemo/Mapkit/AnnotationViewController.h
@@ -9,12 +9,36 @@
 #import "BaseViewController.h"
 #import <MapKit/MapKit.h>
 
+// 自定义标注
 @interface MyAnnotation : NSObject <MKAnnotation>
 
 @property (nonatomic, assign) CLLocationCoordinate2D coordinate;
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subtitle;
 @property (nonatomic, retain) NSString *messages;
+
+- (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate;
+
+- (id)initWithLocation:(CLLocationCoordinate2D)newCoordinate;
+
+@end
+
+@class MyAnnotationView;
+
+@protocol MyAnnotationDelegate <NSObject>
+
+- (void)MyAnnotationDidNavi:(MyAnnotationView *)view;
+
+@end
+
+// 自定义标注视图
+@interface MyAnnotationView : MKAnnotationView {
+    UIView *_contentView;
+}
+
+@property (nonatomic, assign) id<MyAnnotationDelegate> delegate;
+
+- (void)initWithAnnotation:(MyAnnotation *)annotation;
 
 @end
 
@@ -26,6 +50,6 @@
  4、MKPointAnnotation
  5、MKPinAnnotationView
  */
-@interface AnnotationViewController : BaseViewController
+@interface AnnotationViewController : BaseViewController <MyAnnotationDelegate>
 
 @end
--- MapkitDemo/Mapkit/AnnotationViewController.m
+++ MapkitDemo/Mapkit/AnnotationViewController.m
@@ -11,6 +11,108 @@
 
 @implementation MyAnnotation
 
+- (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate {
+    _coordinate = newCoordinate;
+}
+
+- (id)initWithLocation:(CLLocationCoordinate2D)newCoordinate {
+    if (self = [super init]) {
+        _coordinate = newCoordinate;
+    }
+    
+    return self;
+}
+
+@end
+
+@implementation MyAnnotationView
+
+- (id)initWithAnnotation:(id<MKAnnotation>)annotation reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithAnnotation:annotation reuseIdentifier:reuseIdentifier];
+    
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        self.canShowCallout = NO;
+        self.annotation = annotation;
+        [self createViews];
+    }
+   
+    return self;
+}
+
+- (void)createViews {
+    _contentView = [[UIView alloc] init];
+    _contentView.backgroundColor = [UIColor clearColor];
+    
+    CGFloat maxWidth = 235.0f;
+    CGSize addSize1 = [self.annotation.title sizeWithFont:[UIFont systemFontOfSize:14.0f] constrainedToSize:CGSizeMake(maxWidth, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping];
+    CGSize addSize2 = [self.annotation.subtitle sizeWithFont:[UIFont systemFontOfSize:12.0f] constrainedToSize:CGSizeMake(maxWidth, MAXFLOAT) lineBreakMode:NSLineBreakByWordWrapping];
+    float width = (addSize1.width > addSize2.width)?addSize1.width:addSize2.width;
+    width = (width > 80)?width:80;
+    
+    CGRect frame = CGRectMake(0, 0, width + 10 + 65, 162);
+    // 添加下方的标注
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake((frame.size.width - 16) / 2, 56, 16, 33)];
+    imageView.image = [UIImage imageNamed:@"anjuke_icon_itis_position"];
+    [_contentView addSubview:imageView];
+    
+    self.frame = frame;
+    frame = CGRectMake(0, 0, width + 10 + 65, 56);
+    _contentView.frame = frame;
+    
+    // 添加尖角图片
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake((frame.size.width - 16) / 2, 65 - 20, 16, 10)];
+    imageView.image = [UIImage imageNamed:@"wl_map_icon_4"];
+    [_contentView addSubview:imageView];
+    
+    // 上方背景
+    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height - 10)];
+    imageView.image = [[UIImage imageNamed:@"wl_map_icon_5"] stretchableImageWithLeftCapWidth:28 topCapHeight:16];
+    [_contentView addSubview:imageView];
+    
+    // 创建按钮
+    UIButton *btnNavi = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnNavi.frame = CGRectMake(width + 10, 0, 65, 46);
+    [btnNavi setImage:[UIImage imageNamed:@"wl_map_icon_1"] forState:UIControlStateNormal];
+    [btnNavi addTarget:self action:@selector(onClickNavi) forControlEvents:UIControlEventTouchUpInside];
+    [_contentView addSubview:btnNavi];
+    
+    // 创建主标题
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, width, 15)];
+    titleLabel.backgroundColor = [UIColor clearColor];
+    titleLabel.font = [UIFont systemFontOfSize:14.0f];
+    titleLabel.text = self.annotation.title;
+    titleLabel.textColor = [UIColor whiteColor];
+    [_contentView addSubview:titleLabel];
+    
+    // 创建副标题
+    UILabel *subTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 25, width, 15)];
+    subTitleLabel.backgroundColor = [UIColor clearColor];
+    subTitleLabel.font = [UIFont systemFontOfSize:12.0f];
+    subTitleLabel.text = self.annotation.subtitle;
+    subTitleLabel.textColor = [UIColor whiteColor];
+    [_contentView addSubview:subTitleLabel];
+
+    [self addSubview:_contentView];
+}
+
+- (void)initWithAnnotation:(MyAnnotation *)annotation {
+    if (_contentView) {
+        [_contentView removeFromSuperview];
+    }
+    
+    self.annotation = annotation;
+    
+    [self createViews];
+}
+
+- (void)onClickNavi {
+    NSLog(@"-----------onClickNavi");
+    if (_delegate && [_delegate respondsToSelector:@selector(MyAnnotationDidNavi:)]) {
+        [_delegate MyAnnotationDidNavi:self];
+    }
+}
+
 @end
 
 @interface AnnotationViewController () <MKMapViewDelegate> {
@@ -127,6 +229,7 @@
     if ([annotation isKindOfClass:[MyAnnotation class]]) {
         static NSString *myAnnotationIdentifier = @"MyAnnotationIdentifier";
         annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:myAnnotationIdentifier];
+#if 0
         if (annotationView == nil) {
             annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:myAnnotationIdentifier];
             annotationView.canShowCallout = YES;
@@ -137,9 +240,25 @@
         annotationView.image = [UIImage imageNamed:@"startAnnotation"];
         annotationView.frame = CGRectMake(0, 0, 23, 30);
         annotationView.centerOffset = CGPointMake(0, - (annotationView.frame.size.height) / 2);
+#else
+        if (annotationView == nil) {
+            MyAnnotationView *tmpAnnotationView = [[MyAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:myAnnotationIdentifier];
+            tmpAnnotationView.delegate = self;
+            annotationView = tmpAnnotationView;
+        }
+        else {
+            MyAnnotationView *tmpAnnotationView = (MyAnnotationView *)annotationView;
+            [tmpAnnotationView initWithAnnotation:annotation];
+        }
+        
+#endif
     }
     
     return annotationView;
+}
+
+- (void)MyAnnotationDidNavi:(MyAnnotationView *)view {
+    NSLog(@"---------broadcast to main view controller.");
 }
 
 @end
