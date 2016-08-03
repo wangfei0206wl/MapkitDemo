a149ee32742b3e83b15074f2c8943495d4a5c7c5
wangfei <wangfei@hecom.cn>
2016-08-03 10:11:06 +0800

    提交试一下

---
+++ MapkitDemo/Common/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  MapkitDemo
+//
+//  Created by wangfei on 16/8/3.
+//  Copyright © 2016年 busap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ MapkitDemo/Common/TestViewController.m
@@ -0,0 +1,61 @@
+//
+//  TestViewController.m
+//  MapkitDemo
+//
+//  Created by wangfei on 16/8/3.
+//  Copyright © 2016年 busap. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+#pragma mark - Custom Accessors
+
+#pragma mark - Actions
+
+#pragma mark - Public
+
+#pragma mark - Private
+
+#pragma mark - Override
+
+#pragma mark NSObject
+
+#pragma mark - Delegates
+
+#pragma mark - UITextFieldDelegate
+
+#pragma mark - UITableViewDataSource
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - NSCopying
+
+@end
